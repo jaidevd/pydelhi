e1c9eb17d4433e4a23132cbcf744d530bdcaa5f8
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-03-05 18:13:57 +0530

    Second first commit

---
+++ hello.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Cube26 product code
+#
+# (C) Copyright 2015 Cube26 Software Pvt Ltd
+# All right reserved.
+#
+# This file is confidential and NOT open source.  Do not distribute.
+#
+
+"""
+
+"""
+
+
+def hello():
+    # FIXME: Function hello isn't implemented
+    # This raises `NotImplementedError`
+    raise NotImplementedError
+
+
+def recursion():
+    # FIXME: Recursion
+    # Did you mean recursion?
+    return recursion()
